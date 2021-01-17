728b13826776d2e8256509c52742767e0344fc51
propathee <propathee@163.com>
2021-01-18 00:04:48 +0800

    788. Rotated Digits Easy

--- src/com/leetcode/leetcode_788_Rotated_Digits_Easy/Solution788.java
+++ src/com/leetcode/leetcode_788_Rotated_Digits_Easy/Solution788.java
@@ -4,7 +4,7 @@ abstract class Solution788 {
     public abstract int rotatedDigits(int N);
 
     static Solution788 newSolution() {
-        return new S2();
+        return new S3();
     }
 
     private static class S1 extends Solution788 {
@@ -62,6 +62,35 @@ abstract class Solution788 {
                 }
             }
             return cnt + (!anyBad && anyGood ? 1 : 0);
+        }
+    }
+
+    private static class S3 extends Solution788 {
+        private static final int[] MARK = {1,1,2,0,0,2,2,0,1,2};
+
+        @Override
+        public int rotatedDigits(int N) {
+            int cnt = 0;
+            int[] dp = new int[N + 1];
+            for (int i = 0; i <= N; i++) {
+                if (i < 10) {
+                    if (MARK[i] == 2) {
+                        dp[i] = 2;
+                        cnt++;
+                    } else if (MARK[i] == 1) {
+                        dp[i] = 1;
+                    }
+                } else {
+                    int a = dp[i / 10], b = dp[i % 10];
+                    if (a == 1 && b == 1) {
+                        dp[i] = 1;
+                    } else if (a >= 1 && b >= 1) {
+                        dp[i] = 2;
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
         }
     }
 }
